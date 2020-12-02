545e1d998a251a3c072c7aaedfaa03fa59410c3c
SuryaRSoman <suryasurabhi5@gmail.com>
2020-12-02 05:05:38 -0800

    background image added

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
    
     <title>Employee App</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="bg">
     <div class="container">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
